2c1f7cb663179984f91d4512073fefce74e3e41c
Mauro Eijsenring <mauro@eijsenring.com>
2021-08-26 16:41:10 +0200

    added mailsort
    added mailsort

--- automailsorter/automailsorter/Program.cs
+++ automailsorter/automailsorter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit;
 
 namespace automailsorter
 {
@@ -6,7 +9,27 @@ namespace automailsorter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			using (var client = new ImapClient())
+			{
+				client.Connect("imap.strato.com", 993, true);
+
+				client.Authenticate("joey", "password");
+
+				// The Inbox folder is always available on all IMAP servers...
+				var inbox = client.Inbox;
+				inbox.Open(FolderAccess.ReadOnly);
+
+				Console.WriteLine("Total messages: {0}", inbox.Count);
+				Console.WriteLine("Recent messages: {0}", inbox.Recent);
+
+				for (int i = 0; i < inbox.Count; i++)
+				{
+					var message = inbox.GetMessage(i);
+					Console.WriteLine("Subject: {0}", message.Subject);
+				}
+
+				client.Disconnect(true);
+			}
+		}
     }
 }
